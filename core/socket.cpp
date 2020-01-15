2eeb50235c92373dc311e6312d0c2eadac4c6c78
Vlad Petrov <vldptrv@aol.com>
2020-01-15 07:09:56 +0300

    Modify socket.cpp/hpp

--- core/socket.cpp
+++ core/socket.cpp
@@ -13,22 +13,41 @@ struct Socket::Impl
     Impl(int domain, int type, int protocol)
         : handle_(::socket(AF_INET, SOCK_STREAM, 0))
     {
-        if(handle_ < 0) {
+        if (handle_ < 0)
+        {
             throw std::system_error(errno, std::system_category());
         }
     }
 
-    ~Impl(void) noexcept {
-        ::close(handle_);
+    explicit Impl(int handle = -1)
+        : handle_(-1)
+    {
     }
+
+    const int GetHandle(void) const noexcept;
+
+    ~Impl(void) noexcept
+    {
+        if (handle_ >= 0)
+        {
+            ::close(handle_);
+        }
+    }
+
 private:
     int handle_;
 };
 
 Socket::Socket(int domain, int type, int protocol)
-    : impl_(std::make_unique<Impl>(domain, type, protocol))
+    : impl_(std::make_unique<Impl>(domain, type | SOCK_NONBLOCK, protocol))
 {
-
 }
+
+int Socket::GetHandle(void) const noexcept {
+    return impl_->GetHandle();
+}
+
+Socket::Socket(int handle) noexcept
+    : impl_(std::make_unique<Impl>(handle)) {}
 
 } // namespace vldptrv::network
--- core/socket.hpp
+++ core/socket.hpp
@@ -7,8 +7,11 @@ namespace vldptrv::network
 struct Socket
 {
     Socket(int domain, int type, int protocol);
+    int GetHandle(void) const noexcept;
     ~Socket(void) noexcept;
 
+protected:
+    Socket(int handle = -1) noexcept;
 private:
     struct Impl;
     std::unique_ptr<Impl> impl_;
